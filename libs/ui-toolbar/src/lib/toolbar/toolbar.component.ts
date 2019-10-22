927893ffb9f0778518981292b6f7331fe53678fb
david <david@venturplex.com>
2019-10-22 13:13:28 -0700

    created base app and generated neccessary tools to satisfy base app

---
+++ libs/ui-toolbar/src/lib/toolbar/toolbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatSidenav } from '@angular/material';
+
+@Component({
+  selector: 'ui-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
+})
+export class ToolbarComponent implements OnInit {
+  @Input() title: string;
+  @Input() sidenav: MatSidenav;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ libs/ui-toolbar/src/lib/ui-toolbar.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '@task-management-poc/material'
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule
+  ],
+  declarations: [ToolbarComponent]
+})
+export class UiToolbarModule {}
