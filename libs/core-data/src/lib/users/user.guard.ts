986453ec02e03ea7a737b432ccf987bd0695dca0
david <david@venturplex.com>
2019-10-23 06:55:25 -0700

    generated token interceptor

--- libs/core-data/src/lib/users/user.guard.ts
+++ libs/core-data/src/lib/users/user.guard.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Router, CanActivate } from '@angular/router';
+import { UserService } from './user.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserGuard implements  {
-  
+export class UserGuard implements CanActivate {
+
+  constructor(public userService: UserService, public router: Router) { }
+
+  canActivate(): boolean {
+    if (!this.userService.authenticatedUser$.value) {
+      this.router.navigate(['/login'])
+      return false;
+    }
+    return true;
+  }
+
 }
